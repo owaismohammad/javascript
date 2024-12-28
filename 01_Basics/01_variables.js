5cba8cf66c2168e67a17fc890ba7761d7286df1c
owaismohammad <owaismohammad2515@gmail.com>
2024-12-28 20:39:16 +0530

    starting javascript

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =114553; //cannot be changed
+
+// let and var are used to declare variables whereas const is used to store constant
+let accountEmail = "owais@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+let accountState;
+
+// accountId = 2; error***
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121";
+
+console.table([accountEmail, accountId, accountPassword, accountState]);
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs , not browser
+
+let name = "owais";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , is an object
+// undefined => value not assigned, type is undefined
+// symbol => unique
+
+//object
+
+console.log(typeof null);
